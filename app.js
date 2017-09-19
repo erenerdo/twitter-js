e0b6a703874159c5c0a05bb59f9972fb4b229956
Eren Erdogan <ererdogan20@gmail.com>
2017-09-19 10:12:53 -0400

    Finished section 5, Nunjucks

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const app = express(); // creates an instance of an express application
 
-const router = require('./routes/index.js');
+const router = require('./routes');
 const volleyball = require('volleyball');
+
+const nunjucks = require('nunjucks');
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const person1 = new Person('Mike');
+const person2 = new Person('Lebron');
+const person3 = new Person('Kobe');
+
+const peopleArr = [person1, person2, person3];
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render);
+nunjucks.configure('./views/', {noCache: true});
+
+const locals = {
+  title: 'Our Title',
+  people: peopleArr
+};
+
+// nunjucks.render('index.html', locals, function (err, output) {
+//   if (err) throw Error;
+//   console.log(output);
+// });
+
+// res.render('index', locals);
+app.get('/', (req, res) => {
+  res.render('index', locals);
+});
 
 app.listen(3000, () => {
   console.log('listening');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Homepage');
-});
+// router.get('/', (req, res) => {
+//   res.send(nunjucks.render('index.html'));
+//   // var res = nunjucks.render('index.html');
+//   // nunjucks.render('index.html', function(err, res) {
+//   //   if (err) throw err;
+//   // });
+// });
 
 router.get('/news', (req, res) => {
   res.send('News Page ');
