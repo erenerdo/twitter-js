49b7fb156febe5004c9970a762c55e37bfbdc615
Eren Erdogan <ererdogan20@gmail.com>
2017-09-19 15:52:59 -0400

    Finished Forms

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express(); // creates an instance of an express application
 const router = require('./routes');
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
 
 nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render);
+
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Middleware
 app.use(volleyball);
@@ -15,6 +19,8 @@ app.use(volleyball);
 app.use(express.static('public'));
 
 // Routes
+app.use(jsonParser);
+app.use(urlencodedParser);
 app.use(router);
 
 
--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,38 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank.js');
 
+router.post('/tweets', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   // console.log(tweets);
   res.render( 'index', {
-    tweets: tweets
+    tweets: tweets,
+    showForm: true
    });
 });
 
 router.get('/users/:name', (req, res) => {
-  console.log(req.params.name);
+  // console.log(req.params.name);
   // console.log(tweetBank.find({name: req.params.name}));
   res.render('index', {
-    tweets: tweetBank.find({name: req.params.name})
+    tweets: tweetBank.find({name: req.params.name}),
+    showForm: true
   });
 });
 
 router.get('/tweets/:id', (req, res) => {
   let num = Number(req.params.id);
-  console.log(num);
-  console.log(tweetBank.find({id: num}));
+  // console.log(num);
+  // console.log(tweetBank.find({id: num}));
   res.render('index', {
-    tweets: tweetBank.find({id: num})
+    tweets: tweetBank.find({id: num}),
+    showForm: true
   });
 });
 module.exports = router;
